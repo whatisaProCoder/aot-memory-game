78ae5690fbb7b461fef0675ba4d8ce526061b0c2
whatisaProCoder <debnathpritam2005@gmail.com>
2025-12-17 03:04:15 +0530

    fix

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -130,7 +130,7 @@ function GamePage({ levelID }) {
         className="flex flex-col items-center h-full transition-opacity hide-scrollbar"
         style={{ opacity: loading ? 0 : 1 }}
       >
-        <div className="mt-8 flex flex-row justify-center gap-3 max-md:flex-col max-sm:w-full px-4">
+        <div className="mt-8 mb-8 flex flex-row justify-center gap-3 max-md:flex-col max-sm:w-full px-4">
           <div className="flex flex-row items-center gap-3">
             <button
               onClick={() => navigate("/")}
