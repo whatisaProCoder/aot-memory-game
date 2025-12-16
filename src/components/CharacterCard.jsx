81849de27fdd7dbb3f452149b7a66ceb892f9165
whatisaProCoder <debnathpritam2005@gmail.com>
2025-12-17 02:55:06 +0530

    fix

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,11 +2,11 @@ function CharacterCard({ characterObject, onClick }) {
   return (
     <div
       onClick={onClick}
-      className="bg-[#3535357b] backdrop-blur-2xl border border-[#555555] rounded-[1.25rem] p-3 select-none"
+      className="bg-[#3535357b] backdrop-blur-2xl border border-[#555555] rounded-[1.25rem] p-3 select-none overflow-hidden"
     >
       <img
         src={characterObject.img}
-        className="w-[150px] bg-[#6969697a] rounded-[0.7rem]"
+        className="w-[150px] bg-[#6969697a] rounded-[0.7rem] overflow-hidden"
       />
     </div>
   );
