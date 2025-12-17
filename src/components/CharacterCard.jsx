2db4a3a220b505e8df7118b23862cb5c025fd898
whatisaProCoder <debnathpritam2005@gmail.com>
2025-12-17 15:28:37 +0530

    fix

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -7,12 +7,9 @@ function CharacterCard({ characterObject, onClick }) {
       layout="position"
       key={characterObject.id}
       onClick={() => onClick()}
-      className="bg-[#35353589] backdrop-blur-2xl border border-[#555555] rounded-[1.25rem] p-3 max-sm:p-2 select-none overflow-clip"
+      className="border-4 border-[#323232c0] rounded-[1.25rem] select-none overflow-hidden"
     >
-      <img
-        src={characterObject.img}
-        className="w-[150px] bg-[#6969697a] rounded-[0.7rem] overflow-hidden"
-      />
+      <img src={characterObject.img} className="w-[150px]" />
     </motion.div>
   );
 }
