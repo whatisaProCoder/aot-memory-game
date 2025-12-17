e9a25d95974ba1dac9870a2570ab29058b5899ac
whatisaProCoder <debnathpritam2005@gmail.com>
2025-12-17 15:32:43 +0530

    ui improvements

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -7,7 +7,8 @@ function CharacterCard({ characterObject, onClick }) {
       layout="position"
       key={characterObject.id}
       onClick={() => onClick()}
-      className="border-4 border-[#323232c0] rounded-[1.25rem] select-none overflow-hidden"
+      className="border-4 border-[#323232c0] rounded-[1.25rem] select-none overflow-hidden hover:brightness-120"
+      style={{ transition: "filter ease-in-out 0.15s" }}
     >
       <img src={characterObject.img} className="w-[150px]" />
     </motion.div>
